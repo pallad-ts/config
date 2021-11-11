ff70e8e18f19df67a471fae0eecae18cc68bfc46
Łukasz Kużyński <lukasz.kuzynski@gmail.com>
2021-11-11 11:29:11 +0100

    fix: #15 proper type check for

--- packages/main/src/Provider.ts
+++ packages/main/src/Provider.ts
@@ -1,6 +1,12 @@
 import {OptionalPromise} from './utils';
 import {Validation} from 'monet';
 import {ValueNotAvailable} from './ValueNotAvailable';
+import * as is from 'predicates';
+
+const TYPE = '@pallad/config/Provider';
+const TYPE_KEY = '@type';
+
+const IS_TYPE = is.property(TYPE_KEY, is.strictEqualTo(TYPE));
 
 /**
  * Describes config value retrievable from other sources (env, env file, external storage etc)
@@ -8,6 +14,14 @@ import {ValueNotAvailable} from './ValueNotAvailable';
  * @public
  */
 export abstract class Provider<TType> {
+    constructor() {
+        Object.defineProperty(this, TYPE_KEY, {
+            value: TYPE,
+            enumerable: false,
+            configurable: false
+        });
+    }
+
     /**
      * Returns value of provider
      */
@@ -17,7 +31,7 @@ export abstract class Provider<TType> {
      * Checks if given value is a Provider
      */
     static is<T>(value: any): value is Provider<T> {
-        return value instanceof Provider;
+        return value instanceof Provider || IS_TYPE(value);
     }
 }
 
--- packages/main/test/ProviderTest.ts
+++ packages/main/test/ProviderTest.ts
@@ -1,18 +1,60 @@
 import {Provider} from "@src/Provider";
 import {DummyProvider} from "./dummies/DummyProvider";
+import * as fs from 'fs';
+import * as path from 'path';
+import {Validation} from 'monet';
 
 describe('Provider', () => {
     const VALUE = {raw: 'value'} as const;
-    it('testing is Provider', () => {
-        expect(Provider.is(new DummyProvider({
-            value: VALUE,
-            isAsync: false,
-        }))).toBe(true);
+    describe('testing is Provider', () => {
 
-        expect(Provider.is({
-            getValue() {
-                return true
-            }
-        })).toBe(false);
+        it('from local module', () => {
+            expect(Provider.is(new DummyProvider({
+                value: VALUE,
+                isAsync: false,
+            }))).toBe(true);
+
+            expect(Provider.is({
+                getValue() {
+                    return true
+                }
+            })).toBe(false);
+        })
+
+        describe('from copy of module', () => {
+            const FINAL_PATH = path.join(__dirname, '../src/ProviderCopy.ts');
+            const SOURCE_PATH = path.join(__dirname, '../src/Provider.ts');
+            beforeEach(() => {
+                return fs.promises.copyFile(SOURCE_PATH, FINAL_PATH);
+            });
+
+            it('checking instance', () => {
+                // eslint-disable-next-line @typescript-eslint/naming-convention
+                const {Provider: NewProvider} = require(FINAL_PATH);
+
+                const newInstance = new class extends NewProvider {
+                    getValue() {
+                        return Validation.Success(true);
+                    }
+                };
+                const dummy = new DummyProvider({
+                    value: VALUE,
+                    isAsync: false,
+                });
+
+                expect(Provider.is(newInstance)).toBe(true);
+                expect(NewProvider.is(newInstance)).toBe(true);
+                expect(Provider.is(dummy)).toBe(true);
+                expect(NewProvider.is(dummy)).toBe(true);
+
+                expect(NewProvider.is(undefined)).toBe(false);
+                // eslint-disable-next-line no-null/no-null
+                expect(NewProvider.is(null)).toBe(false);
+            });
+
+            afterEach(() => {
+                return fs.promises.unlink(FINAL_PATH);
+            });
+        });
     });
 });
